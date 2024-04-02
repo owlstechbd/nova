ed0a64c7a29b5a3d4e3795c67f5e45f0e9e0509a
owlstechbd <owlstech.net@gmail.com>
2024-04-02 21:56:36 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function showComingSoon() {
+  var comingSoon = document.getElementById("comingSoon");
+  comingSoon.style.display = "block";
+}
+
+function hideComingSoon() {
+  var comingSoon = document.getElementById("comingSoon");
+  comingSoon.style.display = "none";
+}
+
+// animation
+
+  new WOW().init();
